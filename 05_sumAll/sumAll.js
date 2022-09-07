10deb28a2b0692d3d993a116eeacc5f5be558419
fatehss <fatehss@g.ucla.edu>
2022-09-07 15:14:48 -0700

    completed exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,16 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+	if (num1 <0 || num2 <0){return "ERROR";}
+	if (typeof(num1)!= 'number' || typeof(num2)!= 'number'){return "ERROR"}	
+	if (num1%1 != 0 || num2%1 != 0){return "ERROR";}
+	let sum = 0;
+
+	for(let i =Math.min(num1,num2); i<=Math.max(num1,num2); ++i)
+	{
+		sum+= i;
+	}
+
+	return sum;
 
 };
 
