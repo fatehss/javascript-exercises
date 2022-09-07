ed675c03dcc72a9ed0255e5ac2fb83e8aed45fd8
fatehss <fatehss@g.ucla.edu>
2022-09-07 11:42:35 -0700

    finished exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(input) {
 
+	let strArr = input.split('');
+	let output = '';
+	while (strArr.length >0){
+		output += strArr.pop();
+	}
+	return output;
 };
 
 // Do not edit below this line
